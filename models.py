3a2c98e1190a5e82fa1c545acce978ff6cfc914e
emmituuk <emmi.kivivuori@gmail.com>
2023-08-04 22:06:01 +0300

    added comments

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
 
+# create the SQLAlchemy database instance
 db = SQLAlchemy()
--- schema.sql
+++ schema.sql
@@ -1,15 +1,18 @@
+-- table for storing information about different animals
 CREATE TABLE animals(
 animal_id SERIAL PRIMARY KEY,
 animal VARCHAR(250) UNIQUE,
 created_on TIMESTAMP NOT NULL
 );
 
+-- table for storing information about different countries
 CREATE TABLE countries(
 country_id SERIAL PRIMARY KEY,
 country VARCHAR(250) UNIQUE,
 created_on TIMESTAMP NOT NULL
 );
 
+-- table for storing information about persons
 CREATE TABLE person(
 person_id SERIAL PRIMARY KEY,
 first_name VARCHAR(50) NOT NULL,
@@ -19,6 +22,7 @@ own_country_id INTEGER NOT NULL REFERENCES countries(country_id),
 created_on TIMESTAMP NOT NULL
 );
 
+-- table for storing information about a person's favorite animals
 CREATE TABLE favorite_animals(
 person_id INTEGER NOT NULL REFERENCES person(person_id),
 favorite_animal_id INTEGER NOT NULL REFERENCES animals(animal_id),
@@ -26,6 +30,7 @@ other_animal VARCHAR(250),
 created_on TIMESTAMP NOT NULL
 );
 
+-- table for storing information about a person's favorite countries
 CREATE TABLE favorite_countries(
 person_id INTEGER NOT NULL REFERENCES person(person_id),
 favorite_country_id INTEGER NOT NULL REFERENCES countries(country_id),
