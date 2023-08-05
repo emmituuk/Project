f86192a168527fef0cd9fdfd7f809780e639dfcc
emmituuk <emmi.kivivuori@gmail.com>
2023-08-05 14:17:59 +0300

    moved move_to_end function content to add.py

--- functions/other_functions.py
+++ functions/other_functions.py
@@ -1,13 +1,6 @@
 from models import db
 
 
-# moves a list element to the end of the list, required in the drop-down/select menu - used add.py
-def move_to_end(lst, elem):
-    lst.remove(elem)
-    lst.append(elem)
-    return lst
-
-
 # function for validating animal ID - used send.py
 def is_valid_animal_id(text, animal_id):
     sql = text("SELECT COUNT(*) FROM animals WHERE animal_id = :animal_id")
